90d57aa24517b5f2220253e22324407e68293b43
Karl Zander <karl.zander.medicated@gmail.com>
2024-01-13 08:07:09 -0800

    cleanup

--- ssh_remote.py
+++ ssh_remote.py
@@ -24,7 +24,7 @@ def run_ssh_command(
         stdin, stdout, stderr = s.exec_command(command)
         for line in stdout.readlines():
             print(line, end="")
-            logger.info(lines)
+            logger.info(line)
     s.close()
 
 commands = ["ls -l", "uptime"]
@@ -37,9 +37,6 @@ key="/home/iagmla/.ssh/id_ed25519_sk"
 ssh_log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 ssh_log_file = 'ssh_commands.log'
 logger = logging.getLogger(__name__)
-command_logger = logging.StreamHandler()
-formatter = logging.Formatter(ssh_log_format)
-command_logger.setFormatter(formatter)
 logging.basicConfig(
     filename=ssh_log_file,
     filemode='a',
@@ -48,7 +45,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 logger.setLevel(logging.INFO)
-logger.addHandler(command_logger)
 
 
 run_ssh_command(
